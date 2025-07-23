edeece9208bbe72030ec774c8f096a7f911654b8
madokast <578562554@qq.com>
2025-07-23 13:20:41 +0000

    217

---
+++ algorithm/leetcode/q0217/main.cpp
@@ -0,0 +1,19 @@
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s(nums.size());
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            int num = nums[i];
+            auto inserted = s.insert(num);
+            if (!inserted.second) return true;
+        }
+        return false;
+         
+    }
+};
