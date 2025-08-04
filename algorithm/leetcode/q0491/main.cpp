e14403f9eb8d1af45c222caf96f81e209ed02216
zhaorunxiao <zhaorunxiao@sics.ac.cn>
2025-08-04 09:54:08 +0800

    491

--- algorithm/leetcode/q0491/main.cpp
+++ algorithm/leetcode/q0491/main.cpp
@@ -10,6 +10,15 @@ bool v_eq(vector<int>& v1, vector<int>& v2) {
     return true;
 }
 
+bool v_eq_add_one(vector<int>& v1, vector<int>& v2, int num) {
+    if (v1.size() != v2.size()-1) return false;
+    if (v1[v1.size()-1] != num) return false;
+    for (size_t i = 0; i < v2.size()-1; i++) {
+        if (v1[i] != v2[i]) return false;
+    }
+    return true;
+}
+
 unsigned int v_hash(vector<int>& v) {
     unsigned int h = 0;
     for (int e : v) {
@@ -19,43 +28,72 @@ unsigned int v_hash(vector<int>& v) {
     return h;
 }
 
+unsigned int v_hash_add_one(vector<int>& v, int num) {
+    unsigned int h = 0;
+    for (int e : v) {
+        h += static_cast<unsigned int>(e);
+        h *= 33;
+    }
+    h += static_cast<unsigned int>(num);
+    return h;
+}
+
+struct VectorSet {
+    map<unsigned, vector<vector<int>&>> data;
+    pair<bool, unsigned int> exist_v_add_one(vector<int>& v, int num) {
+        unsigned int h = v_hash_add_one(v, num);
+        auto it = this->data.find(h);
+        if (it == this->data.end()) return make_pair(false, h);
+        for (auto& e : (*it).second) {
+            if (v_eq_add_one(e, v, num)) return make_pair(true, h);
+        }
+        return make_pair(false, h);
+    }
+    void put(vector<int>& v, unsigned int h) {
+        this->data[h].push_back(v);
+    }
+};
+
 class Solution {
 public:
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        map<int, vector<pair<unsigned int, vector<int>>>> m;
+        map<int, vector<vector<int>>> m; // last_num -> vectors
+        vector<vector<int>> new_seqs;
+        VectorSet vs;
+        vector<int> empty;
         for (int num : nums) {
-            vector<vector<int>> new_seqs;
+            int new_add_number = 0;
             for (auto &kv : m) {
                 if (kv.first > num) break;
-                for (auto& p : kv.second) {
-                    auto seq = p.second; // do copy
-                    seq.push_back(num);
-                    new_seqs.push_back(move(seq));
-                }
-            }
-            new_seqs.emplace_back(1, num);
-
-            for (vector<int>& seq : new_seqs) {
-                auto h = v_hash(seq);
-                bool has_same = false;
-                for (auto &p : m[num]) {
-                    if (p.first == h && v_eq(seq, p.second)) {
-                        has_same = true;
-                        break;
+                for (auto& v : kv.second) {
+                    auto exist = vs.exist_v_add_one(v, num);
+                    if (!exist.first) {
+                        vector<int> seq{v}; // do copy
+                        seq.push_back(num);
+                        new_seqs.push_back(move(seq));
+                        vs.put(new_seqs[new_seqs.size()-1], exist.second);
+                        new_add_number++;
                     }
                 }
-                if (!has_same) {
-                    m[num].push_back(make_pair(h, move(seq)));
-                }
+            }
+            auto exist = vs.exist_v_add_one(empty, num);
+            if (!exist.first) {
+                new_seqs.emplace_back(1, num);
+                vs.put(new_seqs[new_seqs.size()-1], exist.second);
+                new_add_number++;
+            }
+
+            auto it = --new_seqs.end();
+            for (int i = 0; i < new_add_number; i++) {
+                m[num].push_back(*it);
             }
         }
         
         vector<vector<int>> results;
         for (auto &kv : m) {
-            for (auto &p : kv.second) {
-                auto& seq = p.second;
-                if (seq.size() > 1) {
-                    results.push_back(move(seq));
+            for (auto &v : kv.second) {
+                if (v.size() > 1) {
+                    results.push_back(move(v));
                 }
             }
         }
